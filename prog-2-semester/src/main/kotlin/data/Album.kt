30f9d18ada0aff6faf03e7bde0e8f01605fc2089
Kosty Manuhin <kostymanuhin@gmail.com>
2023-02-13 23:36:58 +0300

    Returned data classes to the original state

--- prog-2-semester/src/main/kotlin/data/Album.kt
+++ prog-2-semester/src/main/kotlin/data/Album.kt
@@ -5,5 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Album(
     val name: String, //Поле не может быть null, Строка не может быть пустой
-    val length: Long = 1  //Значение поля должно быть больше 0
+    val length: Long  //Значение поля должно быть больше 0
 )
