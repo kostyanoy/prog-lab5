70243fea2bcc6f4a866d9a46e24c596ce4be33f2
Kosty Manuhin <kostymanuhin@gmail.com>
2023-02-13 23:21:30 +0300

    FileManager
    -save collection to file as json
    -load collection from file as json
    -read text file

--- prog-2-semester/src/main/kotlin/utils/file/FileManager.kt
+++ prog-2-semester/src/main/kotlin/utils/file/FileManager.kt
@@ -2,21 +2,35 @@ package utils.file
 
 import data.MusicBand
 import utils.Loader
-import utils.Reader
 import utils.Saver
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+import java.lang.StringBuilder
+import java.util.Scanner
 
 class FileManager : Loader, Saver {
-    val serializer: Serializer = SerializeManager();
+    private val serializer: Serializer = SerializeManager()
+    private val file = File("save.txt")
 
-    override fun load(): LinkedHashMap<Int, MusicBand> {
-        TODO("Not yet implemented")
-    }
+    override fun load(): LinkedHashMap<Int, MusicBand> =
+        serializer.deserialize(Scanner(file).nextLine())
 
     override fun save(collection: LinkedHashMap<Int, MusicBand>) {
-        TODO("Not yet implemented")
+        FileOutputStream(file).use { fos ->
+            OutputStreamWriter(fos, Charsets.UTF_8).use { osw ->
+                BufferedWriter(osw).use { bf -> bf.write(serializer.serialize(collection)) }
+            }
+        }
     }
 
     fun readFile(path: String): String {
-        TODO("Not yet implemented")
+        val scanner = Scanner(File(path))
+        return buildString {
+            while (scanner.hasNextLine()){
+                append(scanner.nextLine() + "\n")
+            }
+        }
     }
 }
